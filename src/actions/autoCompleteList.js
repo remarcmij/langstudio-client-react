92ff1c3a7bc0d2bf2f88a387f8f30ecc8bd98e69
Jim Cramer <remarcmij@gmail.com>
2017-06-24 15:27:41 +0200

    added autocomplete cache

--- src/actions/autoCompleteList.js
+++ src/actions/autoCompleteList.js
@@ -1,4 +1,5 @@
 import { Observable } from 'rxjs/Observable'
+import LRU from 'lru-cache'
 
 import config from '../config/config'
 
@@ -9,6 +10,8 @@ export const FETCH_ERROR = PREFIX + 'FETCH_ERROR'
 
 export const fetchAutoCompleteList = term => ({ type: FETCH, term })
 
+const cache = LRU({ max: 100, maxAge: 1000 * 60 * 60 })
+
 const fetchFulfilled = (items) => ({ type: FETCH_FULFILLED, items })
 const fetchError = (error) => ({ type: FETCH_ERROR, error })
 
@@ -16,9 +19,15 @@ export const fetchAutoCompleteListEpic = action$ =>
   action$.ofType(FETCH)
     .debounceTime(250)
     .switchMap(action => {
+      const { term } = action
+      const items = cache.get(term)
+      if (items) {
+        return Observable.of(items)
+      }
       const url = `${config.apiEndPoint}/search/autocomplete?term=${action.term}`
       return Observable.ajax(url)
         .map(res => fetchFulfilled(res.response))
+        .do(items => cache.set(term, items))
         .catch(error => Observable.of(fetchError(error)))
     })
 
