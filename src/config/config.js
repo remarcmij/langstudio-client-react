031e9503f8636428c4e52643d07a20f2436b5abc
Jim Cramer <remarcmij@gmail.com>
2017-06-26 16:08:23 +0200

    use on-line development backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config  = {
-  apiEndPoint: 'http://localhost:9000/api',
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ODE0OGFhNjU3MjU4OTE1MDA1YzQyMDMiLCJpYXQiOjE0OTc1MTk0MTMsImV4cCI6MTUwMDExMTQxM30.Iq9lW7gK0GgNuDhlIs5eKPEO13aGYrhlJnONDskbjx0'
+  apiEndPoint: 'http://indonesisch-dev.taalmap.nl/api',
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTUxMTBmOGY0MWFhZDBhZjhmMDQ0NjgiLCJpYXQiOjE0OTg0ODUwMjQsImV4cCI6MTUwMTA3NzAyNH0.ywoIjFSwvKjyy1XmolW67z7SD4BROJuzAXYIpnXB27w'
 }
 
 export default config
