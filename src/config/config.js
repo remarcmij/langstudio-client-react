26ae96344a24a8f0e4156f563c19c6e60a06e4b8
Jim Cramer <remarcmij@gmail.com>
2017-07-13 11:56:37 +0200

    updated token

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config  = {
   apiEndPoint: 'http://localhost:9000/api',
-  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTU3ZTgzZjU4MmE0ZGVhMDI4MzgxMGIiLCJpYXQiOjE0OTg5MzgyOTIsImV4cCI6MTUwMTUzMDI5Mn0.-j_TC1PgZeJyZeY5K4D9bRfS2C6g9XkdTStwfXpS1E0'
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1OTUxMGYzNDA1MGViZjVhZTBkN2IxNWEiLCJpYXQiOjE0OTk4MDkwNTcsImV4cCI6MTUwMjQwMTA1N30.lAsJCjziVf6X3MLxO-abC5bf3v-JwWVMNVPv4X0A2O8'
 }
 
 export default config
