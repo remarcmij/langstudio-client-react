049457752a3adb81f85ab41d822095fd77f238d7
Jim Cramer <remarcmij@gmail.com>
2017-07-13 11:55:41 +0200

    added support for redux chome extension

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,19 +1,15 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { createEpicMiddleware } from 'redux-observable'
-import logger from 'redux-logger'
 
 import { rootReducer } from './reducers'
 import { rootEpic } from './actions'
 
 const configureStore = () => {
   const epicMiddleware = createEpicMiddleware(rootEpic)
-  // const createStoreWithMiddleware = applyMiddleware(epicMiddleware, logger)(createStore)
-  // return createStoreWithMiddleware(rootReducer)
-
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   return createStore(rootReducer, /* preloadedState, */ composeEnhancers(
-    applyMiddleware(epicMiddleware, logger)
-  ));
+    applyMiddleware(epicMiddleware)
+  ))
 }
 
 export default configureStore
